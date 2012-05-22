d78144e82c5bda1c99e2e858d55a37a80252430e
Jesse Allen <jesse@jessecarl.com>
2012-05-21 19:36:46 -0700

    Add element type
    Adds the element type whose pointer implements the node interface.

---
+++ element.go
@@ -0,0 +1,76 @@
+package goDLX
+
+import "errors"
+
+const (
+	e_element_set_horz = "Only elements can be left or right of element"
+	e_element_set_vert = "Only element or column can be above or below element"
+)
+
+type element struct {
+	left, right, upup, down, colcol node
+}
+
+func (e *element) lft() node {
+	return e.left
+}
+
+func (e *element) rgt() node {
+	return e.right
+}
+
+func (e *element) up() node {
+	return e.upup
+}
+
+func (e *element) dn() node {
+	return e.down
+}
+
+func (e *element) col() node {
+	return e.colcol
+}
+
+func (e *element) setLft(n node) (node, error) {
+	switch n.(type) {
+	case *element:
+		e.left = n
+	default:
+		return nil, errors.New(e_element_set_horz)
+	}
+	return n, nil
+}
+
+func (e *element) setRgt(n node) (node, error) {
+	switch n.(type) {
+	case *element:
+		e.right = n
+	default:
+		return nil, errors.New(e_element_set_horz)
+	}
+	return n, nil
+}
+
+func (e *element) setUp(n node) (node, error) {
+	switch n.(type) {
+	case *column:
+		e.upup = n
+	case *element:
+		e.upup = n
+	default:
+		return nil, errors.New(e_element_set_vert)
+	}
+	return n, nil
+}
+
+func (e *element) setDn(n node) (node, error) {
+	switch n.(type) {
+	case *column:
+		e.down = n
+	case *element:
+		e.down = n
+	default:
+		return nil, errors.New(e_element_set_vert)
+	}
+	return n, nil
+}
